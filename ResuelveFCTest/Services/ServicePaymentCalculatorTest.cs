5a80c5b4526ff4fdd60c9ae806785f06ce247ad5
Beatriz Bravo Gallardo <bravogbeat@outlook.es>
2022-01-14 11:55:07 -0600

    Create test project

---
+++ ResuelveFCTest/Services/ServicePaymentCalculatorTest.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using ResuelveFC.Models;
+using ResuelveFC.Services;
+
+namespace ResuelveFCTest.Services
+{
+    public class Tests
+    {
+        [TestClass]
+        public class ServicePaymentCalculatorTest
+        {
+            [TestMethod]
+             public void Players_Field_Is_Invalid()
+            {
+            }
+        }
+    }
+}
